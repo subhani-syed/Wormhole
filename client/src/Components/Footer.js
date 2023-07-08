3bd8083d449a70f50d04c2efe0e1c8a9664a4aff
Subhani-syed <subhani6352@gmail.com>
2023-07-08 12:12:57 +0530

    Add footer css

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,6 +1,16 @@
 const Footer = () =>{
     return (<>
-        <h1>Footer</h1>
+        <div className="flex justify-between bg-[#FFFF00] border-4 border-black drop-shadow-[4px_4px_0px_black] rounded-[30px] m-10 p-10 ">
+            <div>
+                <a><h1 className="text-xl">How it works</h1></a>
+            </div>
+            <div className="flex">
+                <h1 className="text-xl mx-5">Lets Connect</h1>
+                <a href="https://www.linkedin.com/in/subhani-syed/" target="_blank"><h1 className="text-xl mx-5">LinkedIn</h1></a>
+                <a href="https://github.com/subhani-syed" target="_blank"><h1 className="text-xl mx-5">GitHub</h1></a>
+                <a href="https://twitter.com/subhan1syed" target="_blank"><h1 className="text-xl mx-5">Twitter</h1></a>
+            </div>
+        </div>
     </>)
 }
 export default Footer;
